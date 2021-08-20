a37e7455b80ed45cf0c15031ce39f95b46ef41f1
Navneet <55187546+Navneet102002@users.noreply.github.com>
2021-08-21 01:13:46 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer(){
+    return <footer id='footer'>
+    <div class="container-fluid">
+    <p>© Copyright 2021 The Sparks Foundation</p>
+        <p>Made By Navneet Rai</p>
+    </div>
+        
+    </footer>
+}
+
+export default Footer;
